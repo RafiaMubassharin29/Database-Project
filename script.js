da58e7f3da830809b94008544ba5d2afb276be3e
RafiaMubassharin29 <rmubassharin@gmail.com>
2024-12-22 21:13:09 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+function scrollToSection(id) {
+    const section = document.getElementById(id);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
